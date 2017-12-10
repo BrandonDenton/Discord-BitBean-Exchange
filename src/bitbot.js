34ea35e7f820a4d607365d1fc973648c3465ec67
OinkerBob <oinkerbob1@gmail.com>
2017-12-09 21:37:17 -0500

    added package.json for bitbean dependencies

--- src/bitbot.js
+++ src/bitbot.js
@@ -1,5 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
+//var bitcoin = require('bitcoinjs-lib');
 var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -17,6 +18,7 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
@@ -26,33 +28,44 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		var walletcmd = args[1];
        
         args = args.splice(1);
-        switch(cmd) {
-			case  'yee':
-				bot.sendMessage({
-					to: channelID,
-					message: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
-				});
-			case  'wallet':
-				switch(walletcmd) {
-					case	'help':
+        if (cmd == 'yee') {
+			bot.sendMessage({
+				to: channelID,
+				//message: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
+				message: "ur fuckin gay"
+			});
+		} else if (cmd == 'wallet') {
+			switch(walletcmd) {
+				case	'help':
+					bot.sendMessage({
+						to: channelID,
+						message: walletcmd
+						//message: "Here's how to use the BitBean Exchange bot on this server!\n\t!wallet new <wallet_address>: *Records your wallet address for easy exchange of BitBean by you, server staff, or other users.*\n\t!wallet give <user_tag>: *Gives a server member a set amount of BitBean (you'll be prompted for the amount by the bot to confirm the transaction).*\n\t!wallet request <user_tag>: *Requests a set amoung of BitBean from a user (again, you'll be prompted by a user, pls don't spam this).*\n\n\tIf you have any further questions, please don't hesitate to contact server staff or refer to the BitBean Exchange documentation."
+					}); 
+				case	'new':
+					bot.sendMessage({
+						to: channelID,
+						message: "Thank you, username! Your wallet address has been recorded for ease of trading BitBean."
+					});
+				case	'send':
+					if (args.length != 3) {
 						bot.sendMessage({
 							to: channelID,
-							message: walletcmd
-							//message: "Here's how to use the BitBean Exchange bot on this server!\n\t!wallet new <wallet_address>: *Records your wallet address for easy exchange of BitBean by you, server staff, or other users.*\n\t!wallet give <user_tag>: *Gives a server member a set amount of BitBean (you'll be prompted for the amount by the bot to confirm the transaction).*\n\t!wallet request <user_tag>: *Requests a set amoung of BitBean from a user (again, you'll be prompted by a user, pls don't spam this).*\n\n\tIf you have any further questions, please don't hesitate to contact server staff or refer to the BitBean Exchange documentation."
+							message: "Please enter a BitBean wallet address (!wallet send <WALLET_TO_SEND>)."
 						});
-					case	'new':
+					} else {
+						// BitBean interaction code here
 						bot.sendMessage({
 							to: channelID,
-							message: "Thank you, username! Your wallet address has been recorded for ease of trading BitBean."
+							message: "You just sent <USERNAME> <X> BitBean."
 						});
-					default:
-						bot.sendMessage({
-							to: channelID,
-							message: "bot machine :b:roke"
-						});	
-					break;
-				}
-            break;
-         }
-     }
+					}
+				default:
+					console.log("script machine Broke");
+				break;	
+			}	
+		} else {
+		    console.log("bot ded");
+		}
+    }
 });
