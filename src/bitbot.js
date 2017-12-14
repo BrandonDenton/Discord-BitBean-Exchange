c16ab17f7e13236afdadc1d2a705ae11eb5a81a8
Brandon Denton <brandond@smhh.local>
2017-12-14 16:59:32 -0500

    logging and changed !wallet command to !bitbot for clarity

--- src/bitbot.js
+++ src/bitbot.js
@@ -14,21 +14,29 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 
 //log records on server side
-//var file = new File(config.logfile);
+var file = new File('../config/bitbot.LOG');
 
 //discord.js stuff
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = auth.token;
+const garbage = new Discord.MessageCollector('welcome', );
 
 client.on('ready', () => {		// Le's bOOT IT UP ladies 'n' genlmn!
 	console.log('bitbot ready');
 	// need to figure out how to limit messages by time instead of by quantity
 	//message.channel.fetchMessages({limit: 15}).then(messages => message.channel.bulkDelete(messages));
 	//console.log(client.channels);
+	
+	
+	file.open("w");		// logging
 });
 
+// BitBean Exchange Commands
 client.on('message', message => {
+	//sanity check so the bot doesn't message itself or other bots
+	if (message.author.bot) {return;}
+	
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`	
     if (message.channel.name == "welcome" && message.content.substring(0, 1) == '!') {
@@ -36,12 +44,14 @@ client.on('message', message => {
 		var cmd = args[0];
 		var walletcmd = args[1];
 		
-        if (cmd == 'yee') {
+		switch(cmd):
+			case	'yee':
+        	if (cmd == 'yee') {		// need a "clear" command to wipe chat in case something breaks
 			message.reply("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.");
-		} else if (cmd == 'wallet') {
+		} else if (cmd == 'bitbot') {
 			switch(walletcmd) {
 				case	'help':
-					message.reply("Here's how to use the BitBean Exchange bot on this server!\n\n\t**!wallet new:** DMs you and records your wallet address that you send to the bot privately for easy exchange of BitBean by you, server staff, or other users.\n\t**!wallet give <user_tag>:** Gives a server member a set amount of BitBean (you'll be prompted for the amount by the bot to confirm the transaction).\n\t**!wallet request <user_tag>:** Requests a set amoung of BitBean from a user (again, you'll be prompted by a user, pls don't spam this).\n\nIf you have any further questions, please don't hesitate to contact server staff or refer to the BitBean Exchange documentation.");
+					message.reply("Here's how to use the BitBean Exchange bot on this server!\n\n\t**!bitbot new:** DMs you and records your wallet address that you send to the bot privately for easy exchange of BitBean by you, server staff, or other users.\n\t**!bitbot give <user_tag>:** Gives a server member a set amount of BitBean (you'll be prompted for the amount by the bot to confirm the transaction).\n\t**!bitbot request <user_tag>:** Requests a set amoung of BitBean from a user (again, you'll be prompted by a user, pls don't spam this).\n\nIf you have any further questions, please don't hesitate to contact server staff or refer to the BitBean Exchange documentation.");
 					break;
 				case	'new':
 					//grab the user's BitBean wallet address through a DM
@@ -68,10 +78,12 @@ client.on('message', message => {
 					console.log(message.author.username + " doesn't know how to use the bot, lol.");
 				break;	
 			}	
-		} else {
-		    console.log("bot ded");
-		}
-    }
+		} 
+	}
 });
+
+
+// Chat Clearing Commands
+	//const deleteCount
 
 client.login(token);	//new discord.js login stuff
